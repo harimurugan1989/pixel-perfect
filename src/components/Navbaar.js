4f8184340f49a2e3e93fc13aabe06e8852929740
harimurugan1989 <harimur@gmail.com>
2023-06-04 10:51:43 +0530

    logo updated

--- src/components/Navbaar.js
+++ src/components/Navbaar.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import React from 'react';
-import logo from '../assets/img/rever.jpg';
+import logo from '../assets/img/reverfinal.png';
 import { Nav } from "react-bootstrap";
 import { NavLink, Link } from "react-router-dom";
 
@@ -28,7 +28,7 @@ export const Navbaar = () => {
   return (
     <nav class={scrolled ? "navbar fixed-top scrolled" : "navbar fixed-top"}>
       <div class="container-fluid">
-        <Link to="/"><a class="navbar-brand" ><img src={logo} style={{ width: '40px', height: 'auto' }} alt="Logo" /></a></Link> 
+        <Link to="/"><a class="navbar-brand" ><img src={logo} style={{ width: '220px', height: 'auto' }} alt="Logo" /></a></Link> 
         <div className="nav-options">
 
           {/* <Nav className="ms-auto">
