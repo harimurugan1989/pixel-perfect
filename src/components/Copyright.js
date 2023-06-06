77f652e76446db733a63bc76a1aca79538ddba90
Dr. D. Harimurugan <88608850+harimurugan1989@users.noreply.github.com>
2023-06-06 23:58:22 +0530

    Update Copyright.js
    Official Email updated

--- src/components/Copyright.js
+++ src/components/Copyright.js
@@ -3,7 +3,7 @@ import React from 'react'
 export const Copyright =() =>{
   return (
     <div className='copyright'>
-        <p>ggsingla777@gmail.com, dohitd.it.20@nitj.ac.in</p>
+        <p>pixelperfect@nitj.ac.in</p>
     </div>
   )
 }
